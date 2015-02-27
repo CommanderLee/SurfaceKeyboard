d41850c3f4cdf4d880175081818978985a52d765
Zhen <dao.yi.er.san@gmail.com>
2015-02-27 22:28:32 +0800

    Read csv files

--- DataAnalyzer/analyzer.py
+++ DataAnalyzer/analyzer.py
@@ -1,1 +1,18 @@
-print 'hello world'
+import csv
+
+# Read data
+reader = csv.reader(open("01-29_16_11_49_Typing7.csv"))
+# for _x, _y, _time, _id, _type in reader:
+    # print _x, _y
+lines = [line for line in reader]
+# print lines
+_x = [float(line[0]) for line in lines[1:]]
+_y = [float(line[1]) for line in lines[1:]]
+_time = [float(line[2]) for line in lines[1:]]
+_id = [line[3] for line in lines[1:]]
+_type = [line[4] for line in lines[1:]]
+# print _x
+# print _y
+# print _time
+# print _id
+# print _type

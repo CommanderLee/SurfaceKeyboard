183b4b9347320079ee43d56b2dee9be5fb2a3b0f
Zhen <dao.yi.er.san@gmail.com>
2015-04-24 16:09:54 +0800

    Print to file for JMP analysis

--- DataAnalyzer/gestureAnalyzer.py
+++ DataAnalyzer/gestureAnalyzer.py
@@ -4,6 +4,7 @@
 import Tkinter, tkFileDialog
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 def loadFiles():
     "Load data files of gestures"
@@ -18,13 +19,28 @@ def loadFiles():
     else:
         print 'Error: Cannot Load Files.'
 
+def getDistance(xList, yList):
+    "Calculate distance from (x0,y0) to (xn,yn)"
+
+    if len(xList) > 0 and len(xList) == len(yList):
+        lastX = xList[0]
+        lastY = yList[0]
+        sumDist = 0.0
+        for [x, y] in zip(xList[1:], yList[1:]):
+            sumDist += math.sqrt(math.pow(x - lastX, 2) + math.pow(y - lastY, 2))
+            lastX = x
+            lastY = y
+        return sumDist
+    else:
+        print 'Error: point lists X and Y have different length.'
+
 # Main Procedure
 if __name__ == '__main__':
     openFiles = loadFiles()
     pntColors = ['b.-', 'c.-', 'g.-', 'k.-', 'm', 'c', 'y']
 
-    moveDist = []
-    moveTime = []
+    moveDistList = []
+    moveTimeList = []
     for [openFile, pntColor] in zip(openFiles, pntColors):
         # Read to NumPy Array
         # X, Y, Time, TaskNo-PointNo-FingerId, PointType
@@ -44,7 +60,12 @@ if __name__ == '__main__':
                 if len(relX) > 0:
                     plt.figure(0)
                     plt.plot(relX, relY, pntColor)
-                    print moveTime
+                    # Filter: throw errors
+                    if len(relX) > 3:
+                        moveDistList.append(getDistance(relX, relY))
+                        moveTimeList.append(moveTime)
+                    else:
+                        print 'Warning: Ignore point lists:\n   X:%s\n   Y:%s\n' % (relX, relY)
                     relX = []
                     relY = []
                 # Start a new move
@@ -58,7 +79,10 @@ if __name__ == '__main__':
 
         # Save distance and time to file
         saveFileName = openFile.name.split('.')[0] + '_Result.csv'
-        
+        saveFile = open(saveFileName, 'w')
+        saveFile.write('Distance, Time\n')
+        for [moveDist, moveTime] in zip(moveDistList, moveTimeList):
+            saveFile.write('%f, %f\n' % (moveDist, moveTime))
 
     plt.gca().invert_yaxis()
     plt.show()
